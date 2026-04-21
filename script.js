31200fa8d15836ed8e68a36a7cd8ab9d51766525
Mamadou285 <mamadou.diallo81@login.cuny.edu>
2026-04-21 00:51:42 -0400

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 // Adding key from USDA --- practicing
 
 const API_KEY = 'STWwkht5v3tvyc3fyne7cdTZqB1Z3W4WwuAZKLWe';
-// needs to change
 const query = 'pasta';
 
-const url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${STWwkht5v3tvyc3fyne7cdTZqB1Z3W4WwuAZKLWe}&query=${query}`;
+const url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${API_KEY}&query=${query}`;
 
 async function getRecipes() {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.results);
+
+    const container = document.getElementById('results');
+
+    data.foods.forEach(food => {
+      const div = document.createElement('div');
+      div.textContent = food.description;
+      container.appendChild(div);
+    });
+
   } catch (error) {
     console.error('Error fetching recipes:', error);
   }
 }
+
+getRecipes();
